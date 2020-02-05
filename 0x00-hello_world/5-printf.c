772b06671d9e7939d326ab397f4df2f1b5a98ae0
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-05 18:49:32 +0000

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("with proper grammar, but the outcome is a piece of art,");
-   return 0;
+/**
+* main - entry point
+* Return: always 0 success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
 }
-
