51702824d253a01280c4b1d90367191973c81526
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-05 19:59:03 +0000

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
-* main - entry point
-* Return: always 0 (success)
-*/
-int main(void) {
-    printf("Size of char: %ld byte\n", sizeof(charType));
-    printf("Size of int: %ld bytes\n", sizeof(intType));
-    printf("Size of double: %ld bytes\n", sizeof(doubleType));
-    printf("Size of float: %ld bytes\n", sizeof(floatType));
-    return 0;
+ * main - Entry point
+ * print size of variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %ld byte\n", sizeof(charType));
+printf("Size of int: %ld bytes\n", sizeof(intType));
+printf("Size of double: %ld bytes\n", sizeof(doubleType));
+printf("Size of float: %ld bytes\n", sizeof(floatType));
+return (0);
 }
