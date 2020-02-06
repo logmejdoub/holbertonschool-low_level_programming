8b9c4e5a22c79d1fa95ba27a38b4d3f314916588
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-06 20:54:46 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char current;
+
+	for (current = 'a'; current <= 'z'; current++)
+		putchar(current);
+	for (current = 'A'; current <= 'Z'; current++)
+		putchar(current);
+	putchar('\n');
+	return (0);
+}
+
