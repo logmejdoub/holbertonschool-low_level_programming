97f09bd45fd2a4826cbf5d76b87bd03501cabe83
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-06 22:36:15 +0000

    from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 00 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int current1, current2;
+	for (current1 = 0; current1 <= 9; current1++)
+		for (current2 = 0; current2 <= 9; current2++)
+		{
+			putchar(current1 + '0');
+			putchar(current2 + '0');
+			if ((current1 != 9) || (current2 != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
+
