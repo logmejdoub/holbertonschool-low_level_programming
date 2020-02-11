86b4fef93495874ac0fed2f78ec24bf21ecdaea0
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-11 22:44:55 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,31 @@
  */
 void times_table(void)
 {
-int m, a, c;
-for (a = 0; a <= 9; a++)
-{
-	for (m = 0; m <= 9; m++)
+	int m, a, c;
+
+	for (a = 0; a <= 9; a++)
 	{
-c = (m * a);
-	if (c > 9)
-	{
-		_putchar(' ');
-		_putchar((c / 10) + '0');
-		_putchar((c % 10) + '0');
-	}
-	else if (m != 0)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(c + '0');
-	}
-	else
-	{
-		_putchar(c + '0');
-	}
-	if (m != 9)
-		_putchar(',');
-	}
+		for (m = 0; m <= 9; m++)
+		{
+			c = (m * a);
+			if (c > 9)
+			{
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (m != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+				_putchar(c + '0');
+			if (m != 9)
+				_putchar(',');
+		}
 		_putchar('\n');
-}
+	}
 }
 
