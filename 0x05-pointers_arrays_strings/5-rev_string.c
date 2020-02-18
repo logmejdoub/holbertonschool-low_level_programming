cf79967f06fe6bdc4a195963e3a137404707b75d
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-18 18:50:29 +0000

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* rev_string - prints a string
+* @s: string
+*
+*/
+void rev_string(char *s)
+{
+	char temp[98];
+	int length, current1, current2;
+
+	for (length = 0; s[length] != '\0'; length++)
+		;
+	current1 = length - 1;
+	for (current2 = 0; current2 < length; current1--, current2++)
+		temp[current2] = s[current1];
+	for (current1 = 0; s[current1] != '\0'; current1++)
+		s[current1] = temp[current1];
+}
+
