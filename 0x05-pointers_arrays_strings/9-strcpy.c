4704ed59b434cb1bc6eb9c5147c7a5fce547a320
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-18 21:39:50 +0000

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _strcpy - copies the string pointed to by src
+* @dest: string
+* @src: string
+* Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int current;
+
+	for (current = 0; src[current] != '\0'; current++)
+		dest[current] = src[current];
+	return (src);
+}
+
