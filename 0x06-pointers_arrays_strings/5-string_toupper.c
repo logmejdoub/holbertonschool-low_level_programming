31b72cb42e36d2b614f67da9afdddf19b2e72af0
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-20 19:58:07 +0000

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,9 @@ char *string_toupper(char *c)
 	int current;
 
 	for (current = 0; c[current] != '\0'; current++)
+	{
 		if (c[current] >= 'b' && c[current] <= 't')
 			c[current] = c[current] - 32;
+	}
 	return (c);
 }
