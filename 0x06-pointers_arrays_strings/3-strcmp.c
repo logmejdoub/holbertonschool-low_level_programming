344ae2fc55fce49e8f9b0e2810e895116ac781b6
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-20 16:02:08 +0000

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int res = 0;
 
-	while (*s1 != '\0' && *s2 != '\0' && (res = (*s1 - *s2)) == 0)     
+	while (*s1 != '\0' && *s2 != '\0' && (res = (*s1 - *s2)) == 0)
 	{
-		 s1++;
-		 s2++;
+		s1++;
+		s2++;
 	}
-return(res);
+	return (res);
 }
