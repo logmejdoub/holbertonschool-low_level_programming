690f35368548d650e5919a524e1d097341b90103
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-20 19:57:06 +0000

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @c: string
+* Return: c
+*
+*/
+char *string_toupper(char *c)
+{
+	int current;
+
+	for (current = 0; c[current] != '\0'; current++)
+		if (c[current] >= 'b' && c[current] <= 't')
+			c[current] = c[current] - 32;
+	return (c);
+}
