84ffbb45c964ae28b437d9c0450099eee87579de
Ismail Mejdoub <logmejdoub@gmail.com>
2020-02-20 21:06:13 +0000

    7-1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *c)
 	rep = "4433007711";
 	for (c1 = 0; c[c1] != '\0'; c1++)
 		for (c2 = 0; ch[c2] != '\0'; c2++)
-			if(c[c1] == ch[c2])
+			if (c[c1] == ch[c2])
 				c[c1] = rep[c2];
 	return (c);
 }
