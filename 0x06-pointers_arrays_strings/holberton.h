33af4dc7c9f0d9a9d34cf5eff3300665f51a9a02
Ismail Mejdoub <logmejdoub@gmail.com>
2020-03-01 17:17:42 +0000

    static_libraries

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef holberton_h
 #define holberton_h
 
+void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
